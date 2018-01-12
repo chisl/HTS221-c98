2ce94915ff2ee4ae75a4f8ca8216f4905bd217eb
chisl <info@chisl.io>
2018-01-12 18:58:42 +0100

    Initial commit

---
+++ HTS221.c
@@ -0,0 +1,15 @@
+/*
+ * name:        HTS221
+ * description: Capacitive digital sensor for relative humidity and temperature.
+ * manuf:       ST Microelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/hts221.pdf
+ * date:        2018-01-12
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+
